eb92e9c121872d3520f282a32487e95f95db9c65
Marc Neudert <marc.neudert@gmail.com>
2018-10-29 20:32:50 +0100

    geoip2: 2.7.0 -> 2.9.0

--- packages/python/geoip2/default.nix
+++ packages/python/geoip2/default.nix
@@ -4,12 +4,12 @@ pythonPackages.buildPythonPackage rec {
   python_maxminddb = pkgs.callPackage ../maxminddb { inherit pythonPackages; };
 
   pname   = "geoip2";
-  version = "2.7.0";
+  version = "2.9.0";
   name    = "${pname}-${version}";
 
   src = pythonPackages.fetchPypi {
     inherit pname version;
-    sha256 = "0ia95hjxdk3pa631lyganysl6y5kki82d4hjynv12yv2pn3q551f";
+    sha256 = "1w7cay5q6zawjzivqbwz5cqx1qbdjw6kbriccb7l46p7b39fkzzp";
   };
 
   propagatedBuildInputs = with pythonPackages; [
