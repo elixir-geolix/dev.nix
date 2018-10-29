93014177636d83c4d750050668f5478e05c20464
Marc Neudert <marc.neudert@gmail.com>
2018-10-29 20:32:20 +0100

    maxminddb: 1.3.0 -> 1.4.1

--- packages/python/maxminddb/default.nix
+++ packages/python/maxminddb/default.nix
@@ -2,15 +2,18 @@
 
 pythonPackages.buildPythonPackage rec {
   pname   = "maxminddb";
-  version = "1.3.0";
+  version = "1.4.1";
   name    = "${pname}-${version}";
 
   src = pythonPackages.fetchPypi {
     inherit pname version;
-    sha256 = "1gn4pc143pi8xd9cq2b1xz2i326ics94ilpji5wbnk4bs2v9j8wp";
+    sha256 = "04mpilsj76m29id5xfi8mmasdmh27ldn7r0dmh2rj6a8v2y5256z";
   };
 
-  buildInputs = with pythonPackages; [ nose ];
+  buildInputs = with pythonPackages; [
+    mock
+    nose
+  ];
 
   propagatedBuildInputs = with pythonPackages; [
     ipaddress
