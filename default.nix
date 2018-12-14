92d385ab86d23f53c9ae36a9416d35c94caa92cc
Marc Neudert <marc.neudert@gmail.com>
2018-12-14 16:53:45 +0100

    configures shell-specific HEX_HOME/MIX_HOME

--- default.nix
+++ default.nix
@@ -8,6 +8,11 @@ stdenv.mkDerivation rec {
   };
 
   shellHook = ''
+    export SHELL_DATA_DIR="$(pwd)/runtime"
+
+    export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path '\"$SHELL_DATA_DIR/erlang-history\"'"
+    export HEX_HOME="$SHELL_DATA_DIR/hex"
+    export MIX_HOME="$SHELL_DATA_DIR/mix"
     export PS1="[geolix|\[\e[1m\]\w\[\e[0m\]]$ "
   '';
 
