30519868ed8d971cfb7c719aa0489eddd8ba63dc
Marc Neudert <marc.neudert@gmail.com>
2019-03-24 12:01:30 +0100

    prepares multi-shell process management

--- default.nix
+++ default.nix
@@ -8,12 +8,28 @@ stdenv.mkDerivation rec {
   };
 
   shellHook = ''
+    PROJECT_ROOT="$(pwd)"
+    SHELL_LOCK="$PROJECT_ROOT/runtime/shell.lock"
+    SHELL_NAME="geolix"
+
+    function finish {
+      rm -f "$SHELL_LOCK"
+    }
+
+    if [ ! -f "$SHELL_LOCK" ]; then
+      touch "$SHELL_LOCK"
+
+      SHELL_NAME="$SHELL_NAME|\[\e[1m\]master\[\e[0m\]"
+
+      trap finish EXIT
+    fi
+
     export SHELL_DATA_DIR="$(pwd)/runtime"
 
     export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path '\"$SHELL_DATA_DIR/erlang-history\"'"
     export HEX_HOME="$SHELL_DATA_DIR/hex"
     export MIX_HOME="$SHELL_DATA_DIR/mix"
-    export PS1="[geolix|\[\e[1m\]\w\[\e[0m\]]$ "
+    export PS1="[$SHELL_NAME|\[\e[1m\]\w\[\e[0m\]]$ "
   '';
 
   elixir = pkgs.callPackage ./packages/elixir {};
